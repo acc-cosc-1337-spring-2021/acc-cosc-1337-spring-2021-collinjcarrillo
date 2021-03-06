3e48116bcc3666fb30dfa01d41b02af884ab233b
Github Username <collin.carrillo@g.austincc.edu>
2021-03-06 15:55:48 -0600

    homework 4

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,20 @@
-//cpp
+#include <iostream>
+#include "loops.h"
+using std::cout; using std::cin;
+
+int factorial(int num)
+{
+    
+    unsigned long long factorial = 1;
+
+    
+
+    
+    for(int i = 1; i <=num; ++i) 
+    {
+        factorial *= i;
+        
+    }
+    return factorial;
+
+}
